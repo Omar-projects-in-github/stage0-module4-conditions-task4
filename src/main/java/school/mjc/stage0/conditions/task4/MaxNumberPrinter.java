140ac1602ef278143acd7c62b0e38315c618c593
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-07 22:32:31 +0600

    All tasks have been done

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = first >= second ? (first >= third ? first :
+                third) : (second >= third ?
+                result = second : third);
+        System.out.println(result);
     }
 }
